405950c3ced223ada7face69d8e449d5a68d52c4
guopengsong <guopengsong@autohome.com.cn>
2020-04-15 18:00:23 +0800

    Added： Fixed： Changed： Updated：样式冲突

---
+++ webpack/webpack-demo/app.js
@@ -0,0 +1,6 @@
+import btn from './src/components/button1/button';
+import btn2 from './src/components/button2/button'
+
+btn(1);
+btn(2);
+btn2(3);
---
+++ webpack/webpack-demo/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[{
+                    loader:'style-loader'
+                },{
+                    loader: 'css-loader',
+                    options:{
+                        modules:true,
+                    }
+                }]
+            }
+        ]
+    },
+    mode:'development'
+}
