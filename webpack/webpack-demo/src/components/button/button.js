bbca98d8621ca7e690255a9296d8fb82e2003925
guopengsong <guopengsong@autohome.com.cn>
2020-04-26 18:11:59 +0800

    test

--- webpack/webpack-demo/src/components/button/button.js
+++ webpack/webpack-demo/src/components/button/button.js
@@ -5,7 +5,6 @@ function button() {
     btn_DOM.setAttribute('id', 'btn');
     btn_DOM.className='btn';
     btn_DOM.innerText = '按钮';
-    conso.log('123');
     const root_DOM = document.getElementById('root');
     root_DOM.appendChild(btn_DOM);
 }
