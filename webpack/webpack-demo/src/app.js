e4d0c01b7891e4e4dde8e9b519f2b1118b5f04e8
guopengsong <guopengsong@autohome.com.cn>
2020-05-20 19:21:17 +0800

    清空webpck-demo

--- webpack/webpack-demo/src/app.js
+++
@@ -1,5 +0,0 @@
-import Button from "./components/button/button";
-
-//创建按钮
-Button();
-Button();
--- webpack/webpack-demo/src/components/button/button.js
+++
@@ -1,9 +0,0 @@
-import './button.scss'
-
-export default function Button(text = '按钮') {
-    const button = document.createElement('button');
-    button.innerHTML = text;
-    button.id = 'button';
-    const app = document.getElementById('app');
-    app.appendChild(button);
-}
