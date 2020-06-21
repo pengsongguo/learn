fdd28e1afef1b6ad6ce273d33d39425dd7526bc8
guopengsong <guopengsong@autohome.com.cn>
2020-06-21 19:15:16 +0800

    '删除'

--- webpack/demo7/src/word1.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-
-export default () => {
-    let word1 = document.createElement('div');
-    let wordArr1 = ['床', '前', '明', '月', '光'];
-    word1.innerText = _.join(wordArr1, '~~');
-    document.getElementById('root').appendChild(word1);
-}
