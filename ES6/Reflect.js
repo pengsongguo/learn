ae10c7eea1d71b42052093d056cee244fa775e9d
guopengsong <guopengsong@autohome.com.cn>
2020-08-03 20:59:16 +0800

    Reflect,Promise

--- ES6/Reflect.js
+++ ES6/Reflect.js
@@ -1,11 +1,26 @@
-var obj = {
-    a: 'A',
-    b: 'B'
+// var obj = {
+//     a: 'A',
+//     b: 'B'
+// };
+//
+// // console.log(obj);
+//
+// // console.log(Reflect.has(obj, 'a'));
+// Reflect.set(obj,'c','C');
+// // console.log(obj);
+// console.log(Reflect.get(obj, 'c'));
+
+let obj={
+    a:'A',
+    b:'B'
 };
 
-// console.log(obj);
+console.log(Reflect.get(obj, 'a'));
 
-// console.log(Reflect.has(obj, 'a'));
 Reflect.set(obj,'c','C');
-// console.log(obj);
-console.log(Reflect.get(obj, 'c'));
+console.log(obj);
+
+console.log(Reflect.deleteProperty(obj, 'b'));
+console.log(obj);
+
+console.log(Reflect.getPrototypeOf(obj));
