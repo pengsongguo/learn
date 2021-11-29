bd662d631b99460c170573a5cc1bb174df8e8955
guopengsong <guopengsong@autohome.com.cn>
2021-11-29 14:59:43 +0800

    Added： Fixed： Changed： Updated：

---
+++ learnECMAScript/js/test.js
@@ -0,0 +1,30 @@
+// var a = null;
+// var b;
+
+// console.log(undefined)
+
+// var a = 123;
+// console.log(typeof a);
+// a = a.toString();
+// console.log(typeof a);
+
+// var b = true;
+// console.log(typeof b);
+// b = b.toString();
+// console.log(typeof b);
+// null.toString()
+
+// console.log(typeof String(123))
+
+// var a = null;
+// console.log(typeof a);
+// a = String(a);
+// console.log(typeof a);
+
+var obj = { a: 1 };
+
+console.log(obj);
+var b = String(obj);
+console.log(b);
+var c ="898kk";
+console.log(Number(c));
