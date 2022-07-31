b71d088c01cedadf4eff0ea0808674c017f0842a
guopengsong <albert-guo@outlook.com>
2022-07-31 13:23:41 +0800

    gps

---
+++ createReactApp/my-app/src/App.js
@@ -0,0 +1,12 @@
+import GitHubSearch from "./components/GitHubSearch";
+
+function App() {
+    return (
+        <div className="App">
+            {/*github搜索*/}
+            <GitHubSearch/>
+        </div>
+    );
+}
+
+export default App;
